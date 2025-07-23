326339cc98f80e6afb89fa8007ff39a6559b2816
SangyeopNam <spearoad15@gmail.com>
2025-07-23 15:31:05 -0500

    fix : hibernate 환경설정 파일 config설정으로 이동

--- modules/adapters/out/persistence/src/main/java/com/hoo/auth/adapter/out/persistence/PersistenceConfig.java
+++ modules/adapters/out/persistence/src/main/java/com/hoo/auth/adapter/out/persistence/PersistenceConfig.java
@@ -1,6 +1,8 @@
 package com.hoo.auth.adapter.out.persistence;
 
+import com.hoo.auth.adapter.out.persistence.config.HibernateCustomNamingStrategy;
 import com.hoo.auth.adapter.out.persistence.repository.BusinessUserCredentialJpaRepository;
+import org.hibernate.boot.model.naming.PhysicalNamingStrategy;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -22,5 +24,10 @@ public class PersistenceConfig {
     @Bean
     public PersistenceMapper persistenceMapper() {
         return new PersistenceMapper();
+    }
+
+    @Bean
+    public PhysicalNamingStrategy physicalNamingStrategy() {
+        return new HibernateCustomNamingStrategy();
     }
 }
